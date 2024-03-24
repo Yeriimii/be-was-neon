47dcf7001f2485fcafd89a8e31b726d56794bb6f
Yeriimii <seojs0511@naver.com>
2024-03-25 01:00:57 +0900

    test: UriMapperTest 수정
    - 의존관계 변경에 따른 테스트 내용 수정

--- src/test/java/web/UriMapperTest.java
+++ src/test/java/web/UriMapperTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class UriMapperTest {
 
-    @DisplayName("'/' 또는 '/index.html'을 입력받으면 HtmlProcessor를 반환한다")
+    @DisplayName("'/' 또는 '/index.html'을 입력받으면 HttpProcessor를 반환한다")
     @Test
     void getProcessor_root_processor() {
         // given
@@ -23,9 +23,9 @@ class UriMapperTest {
 
         // then
         assertThat(rootProcessor.isPresent()).isTrue();
-        assertThat(rootProcessor.get()).isInstanceOf(StaticHtmlProcessor.class);
+        assertThat(rootProcessor.get()).isInstanceOf(HttpProcessor.class);
         assertThat(indexProcessor.isPresent()).isTrue();
-        assertThat(indexProcessor.get()).isInstanceOf(StaticHtmlProcessor.class);
+        assertThat(indexProcessor.get()).isInstanceOf(HttpProcessor.class);
     }
 
     @DisplayName("존재하지 않는 URL은 빈 Optional을 반환한다")
