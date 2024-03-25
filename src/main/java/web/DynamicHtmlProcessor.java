fd18c7b29427616dce85983335eb5bdc616a72b9
Yeriimii <seojs0511@naver.com>
2024-03-25 19:05:40 +0900

    refactor: 동적 페이지 생성 방식 변경
    - 기존 하드 코딩 방식에서 템플릿 파일의 일부분을 대체하는 방식으로 변경

--- src/main/java/web/DynamicHtmlProcessor.java
+++ src/main/java/web/DynamicHtmlProcessor.java
@@ -5,13 +5,14 @@ import http.HttpRequest;
 import http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
-import model.User;
 import session.SessionManager;
+import session.SessionManager.SessionUser;
+import utils.ResourceHandler;
 
 public class DynamicHtmlProcessor extends HttpProcessor {
 
     private final SessionManager sessionManager = new SessionManager();
-    private final StringBuilder htmlBuilder = new StringBuilder();
+    public final StringBuilder htmlBuilder = new StringBuilder();
 
     @Override
     public void process(HttpRequest request, HttpResponse response) {
@@ -21,32 +22,18 @@ public class DynamicHtmlProcessor extends HttpProcessor {
         String sessionId = sessionManager.findSessionId(cookies, "SID");
 
         /* 세션 아이디로부터 세션 유저(Optional) 가져오기 */
-        Optional<Object> optionalSession = sessionManager.getSession(sessionId);
+        Optional<SessionUser> sessionUser = sessionManager.getSession(sessionId);
 
-        if (optionalSession.isEmpty()) {
-            /* HTML 작성 */
-            createHtmlHeader();
-            createBodyTopPart();
-            createLoginButton();
-            createBodyCommentPart();
+        /* 기본 HTML 작성 */
+        String htmlTemplate = readTemplate(request.getRequestURI());
+        htmlBuilder.append(htmlTemplate);
 
-            /* http response 작성 */
-            responseHeader200(response, getContentType(request));
-            responseMessage(response, htmlBuilder);
-
-            response.flush();
-            return;
+        /* 세션 유저가 존재하면 user id 표시 */
+        if (sessionUser.isPresent()) {
+            SessionUser user = sessionUser.get();
+            String dynamicHtml = createUserProfile(user.id());
+            changeHtml("<!-- target -->", "<!-- end -->", dynamicHtml);
         }
-
-        /* 세션 아이디에 해당하는 유저 정보 가져오기 */
-        User sessionUser = (User) optionalSession.get();
-        String userName = sessionUser.getName();
-
-        /* HTML 작성 */
-        createHtmlHeader();
-        createBodyTopPart();
-        createUserProfile(userName);
-        createBodyCommentPart();
 
         /* http response 작성 */
         responseHeader200(response, getContentType(request));
@@ -55,168 +42,36 @@ public class DynamicHtmlProcessor extends HttpProcessor {
         response.flush();
     }
 
-    private void createHtmlHeader() {
-        // html header 생성
-        htmlBuilder.append("<!DOCTYPE html>");
-        htmlBuilder.append("<html>");
-        htmlBuilder.append("<head>");
-        htmlBuilder.append("<meta charset=\"UTF-8\" />");
-        htmlBuilder.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
-        htmlBuilder.append("<link href=\"./reset.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("<link href=\"./global.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("<link href=\"./main.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("</head>");
+    public String readTemplate(String templateName) {
+        return ResourceHandler.readTemplate(templateName);
     }
 
-    private void createBodyTopPart() {
-        // html body 생성
-        htmlBuilder.append("<body>");
-        htmlBuilder.append("<div class=\"container\">");
-        htmlBuilder.append("<header class=\"header\">");
-        htmlBuilder.append("<a href=\"/\"><img src=\"./img/signiture.svg\" /></a>");
-        htmlBuilder.append("<ul class=\"header__menu\">");
+    /**
+     * HTML 문자열에서 주어진 시작 문자열(start)과 끝 문자열(end) 사이의 내용을 지우고, 대체할 새로운 템플릿(template)을 삽입한다.
+     *
+     * @param start    대체할 문자열의 시작 부분
+     * @param end      대체할 문자열의 마지막 부분
+     * @param template 대체할 새로운 템플릿 문자열
+     */
+    public void changeHtml(String start, String end, String template) {
+        htmlBuilder.replace(htmlBuilder.indexOf(start) + start.length(), htmlBuilder.indexOf(end), "");
+        htmlBuilder.insert(htmlBuilder.indexOf(start), template);
     }
 
-    private void createLoginButton() {
-        htmlBuilder.append("<li class=\"header__menu__item\" >");
-        htmlBuilder.append("<a class=\"btn btn_contained btn_size_s\" href=\"/login\"> 로그인 </a>");
-        htmlBuilder.append("</li >");
-
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<a class=\"btn btn_ghost btn_size_s\" href=\"/registration\">회원 가입</a>");
-        htmlBuilder.append("</li>");
-    }
-
-    private void createUserProfile(String userId) {
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\" style=\"padding: 15px\">" + userId + "</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<a href=\"/logout\" id=\"logout-btn\" class=\"btn btn_danger btn_size_s\">로그아웃</a>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("</header>");
-    }
-
-    private void createBodyCommentPart() {
-        // body header 닫음
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("</header>");
-
-        // 게시물과 댓글 섹션 추가
-        htmlBuilder.append("<div class=\"wrapper\">");
-        htmlBuilder.append("<div class=\"post\">");
-        htmlBuilder.append("<div class=\"post__account\">");
-        htmlBuilder.append("<img class=\"post__account__img\" />");
-        htmlBuilder.append("<p class=\"post__account__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<img class=\"post__img\" />");
-        htmlBuilder.append("<div class=\"post__menu\">");
-        htmlBuilder.append("<ul class=\"post__menu__personal\">");
-        htmlBuilder.append("<li>");
-        htmlBuilder.append("<button class=\"post__menu__btn\">");
-        htmlBuilder.append("<img src=\"./img/like.svg\" />");
-        htmlBuilder.append("</button>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li>");
-        htmlBuilder.append("<button class=\"post__menu__btn\">");
-        htmlBuilder.append("<img src=\"./img/sendLink.svg\" />");
-        htmlBuilder.append("</button>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("<button class=\"post__menu__btn\">");
-        htmlBuilder.append("<img src=\"./img/bookMark.svg\" />");
-        htmlBuilder.append("</button>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"post__article\">");
-        htmlBuilder.append(
-                "우리는 시스템 아키텍처에 대한 일관성 있는 접근이 필요하며, 필요한 모든 측면은 이미 개별적으로 인식되고 있다고 생각합니다. 즉, 응답이 잘 되고, 탄력적이며 유연하고 메시지 기반으로 동작하는 시스템 입니다. 우리는 이것을 리액티브 시스템(Reactive Systems)라고 부릅니다. 리액티브 시스템으로 구축된 시스템은 보다 유연하고, 느슨한 결합을 갖고, 확장성 이 있습니다. 이로 인해 개발이 더 쉬워지고 변경 사항을 적용하기 쉬워집니다. 이 시스템은 장애 에 대해 더 강한 내성을 지니며, 비록 장애가 발생 하더라도, 재난이 일어나기 보다는 간결한 방식으로 해결합니다. 리액티브 시스템은 높은 응답성을 가지며 사용자 에게 효과적인 상호적 피드백을 제공합니다.");
-        htmlBuilder.append("</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<ul class=\"comment\">");
-        htmlBuilder.append("<li class=\"comment__item\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">");
-        htmlBuilder.append("군인 또는 군무원이 아닌 국민은 대한민국의 영역안에서는 중대한 군사상 기밀·초병·초소·유독음식물공급·포로·군용물에 관한 죄중 법률이 정한 경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다.");
-        htmlBuilder.append("</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"comment__item\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">");
-        htmlBuilder.append("대통령의 임기연장 또는 중임변경을 위한 헌법개정은 그 헌법개정 제안 당시의 대통령에 대하여는 효력이 없다. 민주평화통일자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다.");
-        htmlBuilder.append("</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"comment__item\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">");
-        htmlBuilder.append("민주평화통일자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다.");
-        htmlBuilder.append("</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"comment__item hidden\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">Comment 1</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"comment__item hidden\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">Comment 2</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"comment__item hidden\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\">account</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("<p class=\"comment__item__article\">Comment 3</p>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<button id=\"show-all-btn\" class=\"btn btn_ghost btn_size_m\">");
-        htmlBuilder.append("모든 댓글 보기(3개)");
-        htmlBuilder.append("</button>");
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("<nav class=\"nav\">");
-        htmlBuilder.append("<ul class=\"nav__menu\">");
-        htmlBuilder.append("<li class=\"nav__menu__item\">");
-        htmlBuilder.append("<a class=\"nav__menu__item__btn\" href=\"\">");
-        htmlBuilder.append("<img class=\"nav__menu__item__img\" src=\"./img/ci_chevron-left.svg\" />");
-        htmlBuilder.append("이전 글");
-        htmlBuilder.append("</a>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"nav__menu__item\">");
-        htmlBuilder.append("<a class=\"btn btn_ghost btn_size_m\" href=\"/comment\">댓글 작성</a>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("<li class=\"nav__menu__item\">");
-        htmlBuilder.append("<a class=\"nav__menu__item__btn\" href=\"\">");
-        htmlBuilder.append("다음 글");
-        htmlBuilder.append("<img class=\"nav__menu__item__img\" src=\"./img/ci_chevron-right.svg\" />");
-        htmlBuilder.append("</a>");
-        htmlBuilder.append("</li>");
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("</nav>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</body>");
-        htmlBuilder.append("</html>");
+    private String createUserProfile(String userId) {
+        return String.format("""
+                <li class=\"header__menu__item\">
+                  <div class=\"comment__item__user\">
+                    <img class=\"comment__item__user__img\" />
+                    <p class=\"comment__item__user__nickname\" style=\"padding: 15px\"> %s </p>
+                  </div >
+                </li >
+                <li class=\"header__menu__item\">
+                <a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>
+                </li >
+                <li class=\"header__menu__item\">
+                <a href =\"/logout\" id=\"logout-btn\" class=\"btn btn_danger btn_size_s\">로그아웃</a>
+                </li >
+                """, userId);
     }
 }
--- src/main/java/web/MemberList.java
+++ src/main/java/web/MemberList.java
@@ -15,7 +15,6 @@ import session.SessionManager.SessionUser;
 
 public class MemberList extends DynamicHtmlProcessor {
     private final SessionManager sessionManager = new SessionManager();
-    private final StringBuilder htmlBuilder = new StringBuilder();
 
     @Override
     public void process(HttpRequest request, HttpResponse response) {
@@ -35,12 +34,13 @@ public class MemberList extends DynamicHtmlProcessor {
         SessionUser sessionUser = optionalSession.get();
         String userName = sessionUser.id();
 
-        /* html 테이블 생성 */
-        createHtmlHeader();
-        createBodyTopPart();
-        createUserProfile(userName);
-        createTableHeader();
-        createTableBody(Database.findAll());
+        /* 기본 html 생성 */
+        String template = readTemplate("/user/user-list.html");
+        htmlBuilder.append(template);
+
+        /* 가입된 User List 테이블 추가 */
+        changeHtml("<!-- target user -->", "<!-- end user -->", userName);
+        changeHtml("<!-- target list -->", "<!-- end list -->", createUserTable(Database.findAll()));
 
         /* http response 작성 */
         responseHeader200(response, getContentType(request));
@@ -49,71 +49,8 @@ public class MemberList extends DynamicHtmlProcessor {
         response.flush();
     }
 
-    private void createHtmlHeader() {
-        // html header 생성
-        htmlBuilder.append("<!DOCTYPE html>");
-        htmlBuilder.append("<html>");
-        htmlBuilder.append("<head>");
-        htmlBuilder.append("<meta charset=\"UTF-8\" />");
-        htmlBuilder.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
-        htmlBuilder.append("<link href=\"../reset.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("<link href=\"../global.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("<link href=\"../main.css\" rel=\"stylesheet\" />");
-        htmlBuilder.append("<style>");
-        htmlBuilder.append("table {width: 100%; border-collapse: collapse; margin-top: 20px; box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); border-radius: 10px; overflow: hidden;}");
-        htmlBuilder.append("th, td {border: 1px solid #f0f0f0; padding: 12px; text-align: center; font-size: 14px;}");
-        htmlBuilder.append("th {background-color: #f8f8f8;font-weight: 600;}");
-        htmlBuilder.append("tbody tr:nth-child(even) {background-color: #fafafa;}");
-        htmlBuilder.append("tbody tr:hover {background-color: #f5f5f5;}");
-        htmlBuilder.append("</style>");
-        htmlBuilder.append("</head>");
-    }
-
-    private void createBodyTopPart() {
-        // html body 생성
-        htmlBuilder.append("<body>");
-        htmlBuilder.append("<div class=\"container\">");
-        htmlBuilder.append("<header class=\"header\">");
-        htmlBuilder.append("<a href=\"/\"><img src=\"../img/signiture.svg\" /></a>");
-        htmlBuilder.append("<ul class=\"header__menu\">");
-    }
-
-    private void createUserProfile(String userId) {
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<div class=\"comment__item__user\">");
-        htmlBuilder.append("<img class=\"comment__item__user__img\" />");
-        htmlBuilder.append("<p class=\"comment__item__user__nickname\" style=\"padding: 15px\">" + userId + "</p>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("<li class=\"header__menu__item\">");
-        htmlBuilder.append("<a href=\"/logout\" id=\"logout-btn\" class=\"btn btn_danger btn_size_s\">로그아웃</a>");
-        htmlBuilder.append("</li>");
-
-        htmlBuilder.append("</ul>");
-        htmlBuilder.append("</header>");
-    }
-
-    private void createTableHeader() {
-        htmlBuilder.append("<div class=\"wrapper\">");
-        htmlBuilder.append("<div class=\"page\">");
-        htmlBuilder.append("<h2 class=\"page-title\">회원 리스트</h2>");
-        htmlBuilder.append("<table>");
-        htmlBuilder.append("<thead>");
-        htmlBuilder.append("<tr>");
-        htmlBuilder.append("<th>ID</th>");
-        htmlBuilder.append("<th>이름</th>");
-        htmlBuilder.append("<th>이메일</th>");
-        htmlBuilder.append("</tr>");
-        htmlBuilder.append("</thead>");
-    }
-
-    private void createTableBody(Collection<User> users) {
-        htmlBuilder.append("<tbody>");
+    private String createUserTable(Collection<User> users) {
+        StringBuilder htmlBuilder = new StringBuilder();
 
         /* User List 작성 */
         for (User user : users) {
@@ -124,12 +61,6 @@ public class MemberList extends DynamicHtmlProcessor {
             htmlBuilder.append("</tr>");
         }
 
-        htmlBuilder.append("</tbody>");
-        htmlBuilder.append("</table>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</div>");
-        htmlBuilder.append("</body>");
-        htmlBuilder.append("</html>");
+        return htmlBuilder.toString();
     }
 }
