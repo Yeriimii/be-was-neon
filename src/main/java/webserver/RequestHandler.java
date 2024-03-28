35e564ad3f7f6fee5dea3e3f467778dfa16d12ce
Yeriimii <seojs0511@naver.com>
2024-03-28 14:29:20 +0900

    refactor: RequestHandler 리팩토링
    - 생성자에서 convert 하지않고, try-with-resources 블록에서 필드를 초기화하도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,6 +4,9 @@ import static http.HttpStatus.*;
 import static utils.HttpRequestConverter.*;
 import static utils.HttpResponseConverter.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import http.HttpResponse;
@@ -17,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final Socket connection;
-    private final HttpRequest request;
+    private HttpRequest request;
     private HttpResponse response;
     private final Runnable responseEmpty = () -> {
         response.setHttpVersion("HTTP/1.1");
@@ -31,24 +34,24 @@ public class RequestHandler implements Runnable {
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        request = convertToHttpRequest(connection);
-        response = convertToHttpResponse(connection);
     }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
-        logger.debug("URI = {}", request.getRequestURI());
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            // request + response 분리
+            request = convertToHttpRequest(in);
+            response = convertToHttpResponse(out);
 
             // HttpRequest를 처리할 Processor 찾기
             Optional<HttpProcessor> optionalProcessor = findProcessor(request.getPath());
 
-        // Processor가 존재하면 로직 실행, 없으면 404 status 반환
-        optionalProcessor.ifPresentOrElse(this::handle, responseEmpty);
-    }
-
-    public void handle(HttpProcessor processor) {
-        logger.debug("[REQUEST HANDLER] success service");
-        processor.service(request, response);
+            // Processor가 존재하면 로직 실행, 없으면 404 status 반환
+            optionalProcessor.ifPresentOrElse(processor -> processor.service(request, response), responseEmpty);
+        } catch (IOException e) {
+            logger.error("[REQUEST HANDLER ERROR] {}", e.getMessage());
+        }
     }
 
     public Optional<HttpProcessor> findProcessor(String uri) {
