0721c248dc7537943bbba06b0823524fc866e18d
Yeriimii <seojs0511@naver.com>
2024-03-28 14:23:52 +0900

    refactor: HttpResponseConverter 리팩토링
    - Socket 대신 OutputStream 을 주입 받도록 변경

--- src/main/java/utils/HttpResponseConverter.java
+++ src/main/java/utils/HttpResponseConverter.java
@@ -2,23 +2,17 @@ package utils;
 
 import http.HttpResponse;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.net.Socket;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpResponseConverter {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponseConverter.class);
-    public static HttpResponse convertToHttpResponse(Socket connection) {
-        Optional<HttpResponse> response = Optional.empty();
-        try {
-            OutputStream out = connection.getOutputStream();
-            response = Optional.of(new HttpResponse(new DataOutputStream(out)));
-        } catch (IOException e) {
-            logger.error("[RESPONSE CONVERTER ERROR] {}", e.getMessage());
-        }
+
+    public static HttpResponse convertToHttpResponse(OutputStream out) {
+        Optional<HttpResponse> response = Optional.of(new HttpResponse(new DataOutputStream(out)));
+
         return response.orElseThrow(() -> new IllegalStateException("[RESPONSE CONVERTER ERROR] convert fail"));
     }
 }
